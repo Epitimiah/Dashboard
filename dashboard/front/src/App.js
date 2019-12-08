668913474e07c448b579738e10189971e816a30b
tikystitch <36538825+tikystitch@users.noreply.github.com>
2019-12-08 20:46:18 +0100

    add de la structure globale des widget

--- dashboard/front/src/App.js
+++ dashboard/front/src/App.js
@@ -7,6 +7,8 @@ import {
   Form,
   FormControl,
   Button,
+  Col,
+  Row, 
     } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 /*
@@ -15,29 +17,65 @@ import {
  } from 'reactstrap';
 */
 
-function App() {
+class App extends React.Component {
+  render(){
   return (
-      <Navbar bg="dark" variant="dark">
-    <Navbar.Brand href="#home">
-      <img
-        alt=""
-        src="/logo.svg"
-        width="30"
-        height="30"
-        className="d-inline-block align-top"
-      />{' '}
-      React Bootstrap
-    </Navbar.Brand>
-    <Nav>
-      <Form inline>
-      <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-      <Button variant="outline-light">Search</Button>
-    </Form>
-    </Nav>
     
-  </Navbar>
-    
-  );
-}
+    <div className="App">
+      <header className="App-header">
+   
+        <Navbar fill  bg="dark" variant="dark" className="NavBar">
 
+          
+            <Form inline className="Form"  >
+                <FormControl type="text" placeholder="Search" className="FormControl" />
+                <Button variant="outline-light">Search</Button>
+            </Form>
+           
+        </Navbar>
+      </header>
+
+      <section className="Section">
+        <Row className="Row">
+
+          <Col md={4} className="Col">
+            <Row className="Row">
+              <p> user location </p>
+            </Row>
+            <Row className="Row">
+              <p> Number of sensor used </p>
+            </Row>
+          </Col>
+
+          <Col md={8} className="Col">
+            <p> pie chart </p>
+          </Col>
+        </Row>
+
+        <Row className="Row">
+
+          <Col  md={6} className="Col">
+            <p> bar chart </p>
+          </Col>
+
+          <Col  md={3} className="Col">
+            <p> sensor location </p>
+          </Col>
+
+          <Col className="Col">
+            <Row className="Row">
+              <p> number of persons home </p>
+            </Row>
+            <Row  md={3} className="Row">
+              <p> House size </p>
+            </Row>
+          </Col>
+          
+        </Row>
+      </section>
+
+    </div>
+    );
+  }
+}
 export default App;
